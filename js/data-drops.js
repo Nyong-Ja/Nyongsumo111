9dc754b7ebecfe2e2873c1eae8cd561c3e808728
NyongJa <81202739+Nyong-Ja@users.noreply.github.com>
2026-09-08 14:06:57 +0900

    Add files via upload

---
+++ js/data-drops.js
@@ -0,0 +1,20 @@
+const DROPS_DATA = [
+    {
+        title: "Kinetic Publishing - Twitch Drop",
+        date: "2026. 8. 11.",
+        image: "images/drops/kinetic_hat.webp",
+        content: "키네틱 퍼블리싱 쇼케이스 기념 트위치 드롭스! 파스모 방송 2시간 시청 시 '키네틱 퍼블리싱 모자' 지급.",
+        period: "2026. 8. 14. ~ 2026. 8. 17.",
+        likes: 9,
+        url: "https://kineticgames.co.uk/news"
+    },
+    {
+        title: "Willow - Twitch Drops",
+        date: "2026. 7. 14.",
+        image: "images/drops/cool_cat.webp",
+        content: "윌로우 리워크 기념 트위치 드롭스! 파트너 방송 시청 시 'Cool Cat' ID 카드와 배지 지급.",
+        period: "2026. 7. 21. ~ 2026. 7. 28.",
+        likes: 54,
+        url: "https://kineticgames.co.uk/news"
+    }
+];
